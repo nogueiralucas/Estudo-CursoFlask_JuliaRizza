90af37b2069fd8a17f87dcb4aac6038a99e32276
Lucas Soares Nogueira <lucasn.metalhead@gmail.com>
2020-02-29 22:00:12 +0000

    Adiciona banco de dados e sqlalchemy. Aula 3

--- aula_flask/app/__init__.py
+++ aula_flask/app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+# Configurando banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///storage.db"
+db = SQLAlchemy(app)
 
 from app.controllers import default
