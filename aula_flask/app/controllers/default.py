f9454d078a583c4b7bfa7efef36b62c3d4906587
Lucas Soares Nogueira <lucasn.metalhead@gmail.com>
2020-03-01 15:28:54 +0000

    Modifica rotas e adiciona rota de testes. Aula 06

--- aula_flask/app/controllers/default.py
+++ aula_flask/app/controllers/default.py
@@ -1,7 +1,15 @@
 from app import app
 
 
+@app.route('/index')
 @app.route('/')
 def index():
     return 'Hello World!'
     
+@app.route('/test/', defaults={'name': None})
+@app.route('/test/<name>')
+def teste(name):
+    if name:
+        return f'Olá, {name}!'
+    else:
+        return 'Olá!'
