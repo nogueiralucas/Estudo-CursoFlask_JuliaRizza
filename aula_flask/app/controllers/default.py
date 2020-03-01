d8fa0571945715d5b8803b4ab52a2411812a15c7
Lucas Soares Nogueira <lucasn.metalhead@gmail.com>
2020-03-01 18:46:26 +0000

    Adiciona Templates e Bootstrap - Aula 07

--- aula_flask/app/controllers/default.py
+++ aula_flask/app/controllers/default.py
@@ -1,15 +1,8 @@
 from app import app
+from flask import render_template
 
 
-@app.route('/index')
-@app.route('/')
-def index():
-    return 'Hello World!'
-    
-@app.route('/test/', defaults={'name': None})
-@app.route('/test/<name>')
-def teste(name):
-    if name:
-        return f'Olá, {name}!'
-    else:
-        return 'Olá!'
+@app.route('/index/<user>')
+@app.route('/', defaults={'user': None})
+def index(user):
+    return render_template('index.html', user=user)
