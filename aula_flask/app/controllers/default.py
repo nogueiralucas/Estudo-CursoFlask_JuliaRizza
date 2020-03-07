b3f71d9c22f83eae6fe0ce5955d70ff18bcfab0b
Lucas Soares Nogueira <lucasn.metalhead@gmail.com>
2020-03-07 01:01:57 +0000

    Adiciona formulario de login. - Aula 09

--- aula_flask/app/controllers/default.py
+++ aula_flask/app/controllers/default.py
@@ -1,12 +1,19 @@
 from app import app
 from flask import render_template
 
+from app.models.forms import LoginForm
+
 
 @app.route('/index')
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=["POST"])
 def login():
-    return render_template('base.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+
+    return render_template('login.html', form=form)
